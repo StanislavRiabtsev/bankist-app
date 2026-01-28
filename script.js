c4455aff057178eafbcde0f90e640950ca2780ee
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-01-28 19:34:03 +0100

    Computing Usernames

--- script.js
+++ script.js
@@ -72,17 +72,15 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
-const checkDogs = function (dogsJulia, dogsKate) {
-  const dogsJuliaCorrected = dogsJulia.slice();
-  dogsJuliaCorrected.splice(0, 1);
-  dogsJuliaCorrected.splice(-2);
-  const dogs = dogsJuliaCorrected.concat(dogsKate);
-  dogs.forEach(function (dog, i) {
-    if (dog >= 3) {
-      console.log(`Dog number ${i + 1} is an adult and is ${dog} years old`);
-    } else {
-      console.log(`Dog number ${i + 1} is still a puppy`);
-    }
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
   });
 };
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+createUsernames(accounts);
+console.log(accounts);
