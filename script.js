3e547ca8f5337dee0fc0d7167a017f72f58af250
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-01-29 23:46:31 +0100

    Reduce method

--- script.js
+++ script.js
@@ -83,4 +83,11 @@ const createUsernames = function (accs) {
 };
 
 createUsernames(accounts);
-console.log(accounts);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance}€`;
+};
+calcDisplayBalance(account1.movements);
+
+
