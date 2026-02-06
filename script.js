bf64e1965e46937a8f1e11b5e265931d46ebd658
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-02-06 22:47:42 +0100

    Sorting Arrays

--- script.js
+++ script.js
@@ -57,9 +57,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
-  movements.forEach(function (mov, i) {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
     <div class="movements__row">
@@ -192,4 +195,11 @@ btnClose.addEventListener('click', function (e) {
   }
 
   inputClosePin.value = inputCloseUsername.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currrentAccount.movements, !sorted);
+  sorted = !sorted;
 });
